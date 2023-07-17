d461597646f10c94aa37648fd3367e4debd96008
Andy Nogueira <me@andynogueira.dev>
2023-07-17 16:59:32 -0400

    logic for get latest height streaming using proto client

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"time"
 
 	"github.com/cometbft/cometbft/libs/os"
+	blocksvc "github.com/cometbft/cometbft/proto/tendermint/services/block/v1"
 	client "github.com/cometbft/cometbft/rpc/grpc/client"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(30*time.Second))
 	defer cancel()
 	grpcURI := "0.0.0.0:8080"
 	conn, err := client.New(ctx, grpcURI, client.WithVersionServiceEnabled(true), client.WithInsecure())
@@ -29,7 +33,7 @@ func main() {
 	fmt.Printf("VERSION SERVICE => P2P: %d | Block: %d | ABCI: %s | Node: %s\n", version.P2P, version.Block, version.ABCI, version.Node)
 
 	//// Get Block
-	block, err := conn.GetBlockByHeight(ctx, -10)
+	block, err := conn.GetBlockByHeight(ctx, 10)
 	if err != nil {
 		fmt.Printf("error getting block: %v\n", err)
 		st := status.Convert(err)
@@ -45,4 +49,27 @@ func main() {
 		os.Exit("aborting...")
 	}
 	fmt.Println("BLOCK SERVICE =>", block.Block.String())
+
+	//// Get Latest Height
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+	gRPCConn, err := grpc.Dial("0.0.0.0:8080", opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer gRPCConn.Close()
+
+	gRPCClient := blocksvc.NewBlockServiceClient(gRPCConn)
+	req := blocksvc.GetLatestHeightRequest{}
+	stream, err := gRPCClient.GetLatestHeight(context.Background(), &req)
+	for {
+		height, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.GetLatestHeight(_) = _, %v", gRPCClient, err)
+		}
+		log.Println("New Block:", height)
+	}
 }
